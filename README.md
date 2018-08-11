133b558aaa8577a2c6a35934048cb4d0e6755554
Sarith Subramaniam <sarith@sarith.in>
2018-08-11 19:58:05 +0200

    Function, Json, Readme: 1. Wrote a function to create the json file containing fonts. 2. Updated the readme.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fontLib
-A font library for use in data visualizations.
+
+(A font library for use in data visualizations. This public repository has been created
+for very specific private uses and isnt really intended for public dissemination. The
+information on this page are mostly notes-to-self.)
+
+#### Initial Commit Notes
+Right now, this repo contains a bunch of fonts from Windows and Google fonts. I might add
+other fonts based on requirements. Only tested with ttf files.
+The json file in the root directory (fontNames.json) relates the ttf files stored in the fonts
+directory to the human readable names.
+This json file can be regenerated using the createJson.py
+script. The createJson.py script requires fontTools (https://github.com/fonttools/fonttools).
+
--- createJson.py
+++ createJson.py
@@ -1,12 +1,27 @@
-import sys
+"""
+Use the function createJsonFile to update fontNames.json in the root directory.
+"""
 from fontTools import ttLib
 import os
+import json
 
+def __extractFontName(font, FONT_SPECIFIER_NAME_ID = 4, FONT_SPECIFIER_FAMILY_ID = 1):
+    """
 
-FONT_SPECIFIER_NAME_ID = 4
-FONT_SPECIFIER_FAMILY_ID = 1
-def shortName( font ):
-    """Get the short name from the font's names table"""
+    :param font: An instance of the class ttlib.TTFont
+    :type font: ttLib.TTFont
+    :param FONT_SPECIFIER_NAME_ID: Defaults to 4
+    :param FONT_SPECIFIER_FAMILY_ID: Defaults to 1
+    :return: A tuple containing the name of the font and the family of the font.
+
+    Usage:
+        ttfFile=someFont.tff
+        tt=ttLib.TTFont(ttfFile)
+        name,family=shortname(tt)
+
+    This function was mostly copied from an existing code snippet on github. Credit to
+    the original author.
+    """
     name = ""
     family = ""
     for record in font['name'].names:
@@ -25,9 +40,38 @@ def shortName( font ):
     return name, family
 
 
+def createJsonFile(jsonPath="fontNames.json",fontDirectory="fonts"):
+    """
 
-for idx,fileType in enumerate(os.listdir("fonts")):
-    fileName=os.path.join("fonts",fileType)
-    if os.path.isfile(fileName):
-        tt =ttLib.TTFont(fileName)
-        print(shortName(tt))
+    :param jsonPath: Path where the json file is to be written.
+    :param fontDirectory: The directory where the fonts are stored.
+    :return:
+
+    Instead of storing absolute or relative paths, I am storing the directory and the
+    fontfiles as two separate entries in the json.
+    """
+
+    abbDict={"narrow":"Nar","italic":"Ita","bold":"Bld","regular":"Reg","style":"",
+             "condensed":"Cnd","light":"Lig"}
+
+    outputDict={}
+    outputDict["directory"]=fontDirectory
+    outputDict["fonts"]={}
+    for idx,fileType in enumerate(os.listdir(fontDirectory)):
+        fileName=os.path.join(fontDirectory,fileType)
+        if os.path.isfile(fileName):
+
+            font =ttLib.TTFont(fileName)
+            name,family=__extractFontName(font)
+            for key,value in abbDict.items():
+                if key in name:
+                    name=name.replace(key,value)
+                if key.capitalize() in name:
+                    name = name.replace(key.capitalize(), value)
+            outputDict["fonts"][name.replace(" ","")]=os.path.split(fileName)[-1]
+
+    with open(jsonPath,"w") as jsonOutputData:
+        json.dump(outputDict,jsonOutputData)
+
+if __name__ == "__main__":
+    createJsonFile()
